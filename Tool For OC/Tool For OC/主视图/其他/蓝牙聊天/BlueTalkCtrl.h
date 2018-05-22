374ae672d94e58e37621f88b7aa18f91b6f257fa
CYC666 <13705038428@163.com>
2018-05-22 09:44:07 +0800

    11

---
+++ Tool For OC/Tool For OC/主视图/其他/蓝牙聊天/BlueTalkCtrl.h
@@ -0,0 +1,13 @@
+//
+//  BlueTalkCtrl.h
+//  Tool For OC
+//
+//  Created by 曹老师 on 2018/5/22.
+//  Copyright © 2018年 曹老师. All rights reserved.
+//
+
+#import "BaseController.h"
+
+@interface BlueTalkCtrl : BaseController
+
+@end
